cf8fa2585098b85a9acb3783b8855fa8cb7d7bba
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-20 23:34:48 -0300

    FileSystemNode interface

---
+++ src/main/java/edu/austral/ingsis/clifford/filesystem/node/FileSystemNode.java
@@ -0,0 +1,11 @@
+package edu.austral.ingsis.clifford.filesystem.node;
+
+public interface FileSystemNode extends Comparable<FileSystemNode> {
+    String name();
+
+    boolean isDirectory();
+
+    default int compareTo(FileSystemNode o) {
+        return name().compareTo(o.name());
+    }
+}
