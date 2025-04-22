fe599bc27cd8cd6986bfdf5978099f36b1721e21
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-22 13:51:12 -0300

    Rm command implementation, so that it can delete either the file or directory sent (in this last case, the flag --recursive must be sent too)

---
+++ src/main/java/edu/austral/ingsis/clifford/command/RmCommand.java
@@ -0,0 +1,35 @@
+package edu.austral.ingsis.clifford.command;
+
+import edu.austral.ingsis.clifford.filesystem.FileSystem;
+import edu.austral.ingsis.clifford.filesystem.node.DirectoryNode;
+import edu.austral.ingsis.clifford.filesystem.node.FileSystemNode;
+
+import java.util.Optional;
+
+public class RmCommand implements Command {
+    @Override
+    public String execute(FileSystem fileSystem, String argument) {
+        DirectoryNode currentDirectory = fileSystem.getCurrent();
+
+        if (argument.startsWith("--recursive")) {
+            String childName = argument.substring(12).trim();
+            if (currentDirectory.hasChild(childName)) {
+                currentDirectory.removeChild(childName);
+                return "'" + childName + "' removed";
+            } else {
+                return "File or directory not found";
+            }
+        }
+
+        Optional<FileSystemNode> possibleChild = currentDirectory.getChild(argument);
+        if (possibleChild.isPresent()) {
+            FileSystemNode child = possibleChild.get();
+            if (child.isDirectory()) {
+                return "Cannot remove '" + argument + "', is a directory";
+            }
+            currentDirectory.removeChild(argument);
+            return "'" + argument + "' removed";
+        }
+        return "File or directory not found";
+    }
+}
