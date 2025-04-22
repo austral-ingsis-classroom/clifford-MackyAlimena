ec6d575a903f55a127d8b9f2b37d1ae94f6ac076
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-22 11:53:59 -0300

    Ls command implementation, so that it can return de files in directory either in asc, desc, or the order they have been created

---
+++ src/main/java/edu/austral/ingsis/clifford/command/LsCommand.java
@@ -0,0 +1,28 @@
+package edu.austral.ingsis.clifford.command;
+
+import edu.austral.ingsis.clifford.filesystem.FileSystem;
+import edu.austral.ingsis.clifford.filesystem.node.FileSystemNode;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LsCommand implements Command {
+    @Override
+    public String execute(FileSystem fileSystem, String argument) {
+        List<FileSystemNode> children = fileSystem.getCurrent().getChildren();
+
+        if (children.isEmpty()) return "";
+
+        // Parse the argument for sorting order
+        if ("--ord=asc".equals(argument)) {
+            children.sort(Comparator.naturalOrder());
+        } else if ("--ord=desc".equals(argument)) {
+            children.sort(Comparator.reverseOrder());
+        }
+        // Else, return in the creation order
+        return children.stream()
+                .map(FileSystemNode::name)
+                .collect(Collectors.joining(" "));
+    }
+}
