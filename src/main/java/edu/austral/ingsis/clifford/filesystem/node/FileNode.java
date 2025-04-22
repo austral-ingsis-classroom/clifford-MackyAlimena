879b2ea441e0e90b5b8847d2515a9bc67a181fdf
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-22 09:00:43 -0300

    implement FileNode

---
+++ src/main/java/edu/austral/ingsis/clifford/filesystem/node/FileNode.java
@@ -0,0 +1,18 @@
+package edu.austral.ingsis.clifford.filesystem.node;
+
+public class FileNode implements FileSystemNode {
+    private final String name;
+
+    public FileNode(String name) {
+        this.name = name;
+    }
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+}
