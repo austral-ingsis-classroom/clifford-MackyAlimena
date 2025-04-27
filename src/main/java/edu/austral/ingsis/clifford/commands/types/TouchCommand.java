be2bb820be37c38f82211625dabf3eba9f354aca
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-27 19:44:21 -0300

    Updating Touch to be immutable and use Result

--- src/main/java/edu/austral/ingsis/clifford/commands/types/TouchCommand.java
+++ src/main/java/edu/austral/ingsis/clifford/commands/types/TouchCommand.java
@@ -1,33 +1,35 @@
 package edu.austral.ingsis.clifford.commands.types;
 
+import edu.austral.ingsis.clifford.commands.result.Result;
 import edu.austral.ingsis.clifford.filesystem.FileSystem;
 import edu.austral.ingsis.clifford.filesystem.node.DirectoryNode;
 import edu.austral.ingsis.clifford.filesystem.node.FileNode;
 
 public class TouchCommand implements Command {
-  @Override
-  public String execute(FileSystem fileSystem, String argument) {
-    if (!isValidName(argument)) {
-      return "Invalid file name";
+    @Override
+    public Result execute(FileSystem fileSystem, String argument) {
+        if (!isValidName(argument)) {
+            return Result.error(fileSystem, "Invalid file name");
+        }
+
+        DirectoryNode currentDir = fileSystem.getCurrent();
+
+        if (alreadyExists(currentDir, argument)) {
+            return Result.error(fileSystem, "File already exists");
+        }
+
+        FileNode newFile = new FileNode(argument);
+        DirectoryNode updatedDir = currentDir.addChild(newFile);
+        FileSystem updatedFs = fileSystem.setCurrent(updatedDir);
+
+        return Result.success(updatedFs, "'" + argument + "' file created");
     }
 
-    DirectoryNode currentDir = fileSystem.getCurrent();
-
-    if (alreadyExists(currentDir, argument)) {
-      return "File already exists";
+    private boolean isValidName(String name) {
+        return name != null && !name.contains("/") && !name.contains(" ");
     }
 
-    FileNode newFile = new FileNode(argument);
-    currentDir.addChild(newFile);
-
-    return "'" + argument + "' file created";
-  }
-
-  private boolean isValidName(String name) {
-    return name != null && !name.contains("/") && !name.contains(" ");
-  }
-
-  private boolean alreadyExists(DirectoryNode currentDir, String argument) {
-    return currentDir.hasChild(argument);
-  }
+    private boolean alreadyExists(DirectoryNode currentDir, String argument) {
+        return currentDir.hasChild(argument);
+    }
 }
