8cea63269979a67fad80590eb63bfb97c403dfbf
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-27 19:20:39 -0300

    Result logic to modularize string returns

---
+++ src/main/java/edu/austral/ingsis/clifford/commands/result/Result.java
@@ -0,0 +1,40 @@
+package edu.austral.ingsis.clifford.commands.result;
+
+import edu.austral.ingsis.clifford.commands.result.ResultType;
+import edu.austral.ingsis.clifford.filesystem.FileSystem;
+
+public class Result {
+    private final FileSystem fileSystem;
+    private final String message;
+    private final ResultType type;
+
+    public Result(FileSystem fileSystem, String message, ResultType type) {
+        this.fileSystem = fileSystem;
+        this.message = message;
+        this.type = type;
+    }
+
+    public FileSystem getFileSystem() {
+        return fileSystem;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ResultType getType() {
+        return type;
+    }
+
+    public static Result success(FileSystem fs, String msg) {
+        return new Result(fs, msg, ResultType.SUCCESS);
+    }
+
+    public static Result error(FileSystem fs, String msg) {
+        return new Result(fs, msg, ResultType.ERROR);
+    }
+
+    public static Result info(FileSystem fs, String msg) {
+        return new Result(fs, msg, ResultType.INFO);
+    }
+}
---
+++ src/main/java/edu/austral/ingsis/clifford/commands/result/ResultType.java
@@ -0,0 +1,7 @@
+package edu.austral.ingsis.clifford.commands.result;
+
+public enum ResultType {
+    SUCCESS,
+    ERROR,
+    INFO
+}
