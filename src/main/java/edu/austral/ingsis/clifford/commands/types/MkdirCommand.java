06ff8fcc5943243bf236a177ed5b7780d078bd5a
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-27 19:44:07 -0300

    Updating MKDIR to be immutable and use Result

--- src/main/java/edu/austral/ingsis/clifford/commands/types/MkdirCommand.java
+++ src/main/java/edu/austral/ingsis/clifford/commands/types/MkdirCommand.java
@@ -1,32 +1,39 @@
 package edu.austral.ingsis.clifford.commands.types;
 
+import edu.austral.ingsis.clifford.commands.result.Result;
 import edu.austral.ingsis.clifford.filesystem.FileSystem;
 import edu.austral.ingsis.clifford.filesystem.node.DirectoryNode;
 
+import java.util.List;
+
+
 public class MkdirCommand implements Command {
-  @Override
-  public String execute(FileSystem fileSystem, String argument) {
-    if (!isValidName(argument)) {
-      return "Invalid directory name";
+    @Override
+    public Result execute(FileSystem fileSystem, String argument) {
+        if (!isValidName(argument)) {
+            return Result.error(fileSystem, "Invalid directory name");
+        }
+
+        DirectoryNode currentDir = fileSystem.getCurrent();
+
+        if (alreadyExists(currentDir, argument)) {
+            return Result.error(fileSystem, "Directory already exists");
+        }
+
+        DirectoryNode newDirectory = new DirectoryNode(argument, currentDir, List.of());
+        DirectoryNode updatedDir = currentDir.addChild(newDirectory);
+        FileSystem updatedFs = fileSystem.setCurrent(updatedDir);
+
+        return Result.success(updatedFs, "'" + argument + "' directory created");
     }
 
-    DirectoryNode currentDir = fileSystem.getCurrent();
-
-    if (alreadyExists(currentDir, argument)) {
-      return "Directory already exists";
+    private boolean isValidName(String name) {
+        return name != null && !name.contains("/") && !name.contains(" ");
     }
 
-    DirectoryNode newDirectory = new DirectoryNode(argument, currentDir);
-    currentDir.addChild(newDirectory);
-
-    return "'" + argument + "' directory created";
-  }
-
-  private boolean isValidName(String name) {
-    return name != null && !name.contains("/") && !name.contains(" ");
-  }
-
-  private boolean alreadyExists(DirectoryNode directory, String name) {
-    return directory.hasChild(name);
-  }
+    private boolean alreadyExists(DirectoryNode directory, String name) {
+        return directory.hasChild(name);
+    }
 }
+
+
